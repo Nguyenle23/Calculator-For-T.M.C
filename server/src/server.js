5c7545476798be2e29fdfe61724acf23c07c6dbf
Nguyenle23 <beenbin2308@gmail.com>
2022-11-29 12:33:10 +0700

    Custom domain for server

--- server/src/server.js
+++ server/src/server.js
@@ -2,15 +2,18 @@ const express = require('express');
 const app = express();
 const port = 4000 || process.env.PORT;
 const route = require('./routes');
-const cors = require('cors');
 
-app.use(express.json());
-app.use(cors());
+const cors = require('cors');
+const corsOptions = require('./config/domain');
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 })
+
+app.use(express.json());
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
     res.send('RESTfull API successfully called');
