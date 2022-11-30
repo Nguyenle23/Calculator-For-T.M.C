5fe454fa9ac4960151800a9fc763237fa800155d
namanh2310 <101866057+namanh2310@users.noreply.github.com>
2022-11-30 16:41:05 +0700

    Generate Home page with navigation

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./css/Global/App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./component/Header/Header";
+import Home from "./Page/Home";
 import GoldenSectionSearch from "./Optimize/GoldenSectionSearch/GoldenSectionSearch";
 import Bisection from "./Optimize/Bisection/Bisection";
 
@@ -11,10 +12,15 @@ export default function App() {
       <Router>
         <Header />
         <Routes>
+          <Route path="/" element={<Home />}></Route>
+
+          {/* golden section search */}
           <Route
             path="/golden-section"
             element={<GoldenSectionSearch />}
           ></Route>
+
+          {/* bisection method */}
           <Route path="/bisection" element={<Bisection />}></Route>
         </Routes>
       </Router>
---
+++ client/src/Page/Home.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+function Home() {
+  const navList = [
+    {
+      navName: "Golden Section Search",
+      navPath: "golden-section",
+    },
+
+    {
+      navName: "Bisection Method",
+      navPath: "bisection",
+    },
+  ];
+  const navigator = useNavigate();
+  return (
+    <div className="page home-container">
+      {navList.map((nav, index) => (
+        <ul className="home-nav-list">
+          <li onClick={() => navigator(`/${nav.navPath}`)} className="home-nav">
+            {nav.navName}
+          </li>
+        </ul>
+      ))}
+    </div>
+  );
+}
+
+export default Home;
